19f5dbc6e4d97a8294f98624261d36ab7c31c55e
Diego G.Lins <dl7designer@gmail.com>
2023-06-05 20:57:22 -0300

    deploy

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import express, {Request,Response} from "express"
 import { users } from "./database/users";
 import { userRoutes } from "./routes/user.routes";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
+dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 
 app.use("/user", userRoutes());
 
-app.listen(3333, () => {
+app.listen(process.env.PORT, () => {
     console.log(" Api rodando ...");
     
 })
